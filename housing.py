a3a24fda610fda0a08a37eb1f9c9d7a28931aa1b
Arturo Carlos Rodriguez Cobo <art_rg@hotmail.com>
2019-11-30 17:04:00 +0100

    Puesta al día

--- housing.py
+++ housing.py
@@ -17,12 +17,12 @@ housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
 # el segundo argumento indica qué se hace con lo que no cumpla la primera condición. Es decir, LOS QUE NO SEAN MENORES QUE 5 (los mayores o iguales) SE PONEN A 5.0
 housing["income_cat"].where(housing["income_cat"] < 5,5.0, inplace = True)
 
-print(housing.describe())
-print(housing.head(100))
-housing.info()
-print(housing['ocean_proximity'].value_counts())
-housing.hist(bins = 50, figsize=(20,15))
-plt.show()
+# print(housing.describe())
+# print(housing.head(100))
+# housing.info()
+# print(housing['ocean_proximity'].value_counts())
+# housing.hist(bins = 50, figsize=(20,15))
+# plt.show()
 # train_set, test_set = train_test_split(housing, test_size = 0.2, random_state = 42)
 
 split = StratifiedShuffleSplit(n_splits=1, test_size= 0.2 , random_state= 42)
@@ -43,6 +43,7 @@ housing = strat_train_set.copy()
 # housing.plot(kind="scatter", x="longitude",y="latitude", alpha=0.4, s=housing["population"]/100, label="population",c="median_house_value", cmap=plt.get_cmap("jet"),colorbar=True)
 # plt.legend()
 # plt.show()
+# print(housing.head(100))
 # corr_matrix = housing.corr()
 # print(corr_matrix["median_house_value"].sort_values(ascending = False))
 
@@ -50,15 +51,22 @@ housing = strat_train_set.copy()
 # scatter_matrix(housing[attributes])
 # plt.show()
 
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+# corr_matrix = housing.corr()
+# print(housing.head(100))
+# print(corr_matrix["median_house_value"].sort_values(ascending = False))
 housing = strat_train_set.drop("median_house_value", axis=1)
 housing_labels = strat_train_set["median_house_value"].copy()
 
-imputer = Imputer(strategy="median")
-housing_num = housing.drop("ocean_proximity", axis=1)
-imputer.fit(housing_num)
+imputer = Imputer(strategy="median") # creamos la instancia
+housing_num = housing.drop("ocean_proximity", axis=1) # Imputer solo puede trabajar con datos numéricos
+imputer.fit(housing_num) # "Entrenamos" el Imputer. Simplemente calcula la mediana
 # print(imputer.statistics_)
 # print(housing_num.median().values)
-X = imputer.transform(housing_num)
-housing_tr = pd.DataFrame(X, columns= housing_num.columns)
+X = imputer.transform(housing_num) # transformamos el dataset con el modelo obtenido. Es decir, en los missing values metemos la mediana de la feature
+housing_tr = pd.DataFrame(X, columns= housing_num.columns) # le volvemos a poner los headers a X, ya que transform devuelve sólo las filas de datos (plain Numpy array)
 # En este punto, seguimos sin "ocean_proximity"
 # print(housing_tr.info())
+
